2a1f67403e3cabbd79ce6c95d682d3eae278e494
Piotr Lizonczyk <piotr.lizonczyk@gmail.com>
2017-03-25 12:01:04 +0100

    purged

--- carousel/hackathon/admin.py
+++ carousel/hackathon/admin.py
@@ -1,15 +1,6 @@
 from django.contrib import admin
-from hackathon.models import UserProfile, Profile, InstagramProfile, TwitterProfile, MeetupToken, GithubProfile, LinkedinProfile, TumblrProfile
-
-# Register your models here.
-class TwitterProfileAdmin(admin.ModelAdmin):
-	list_display = ('user','twitter_user')
+from hackathon.models import UserProfile, Profile, LinkedinProfile
 
 admin.site.register(UserProfile)
 admin.site.register(Profile)
-admin.site.register(InstagramProfile)
-admin.site.register(TwitterProfile, TwitterProfileAdmin)
-admin.site.register(GithubProfile)
-admin.site.register(MeetupToken)
 admin.site.register(LinkedinProfile)
-admin.site.register(TumblrProfile)
--- carousel/hackathon/forms.py
+++ carousel/hackathon/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django import forms
 from hackathon.models import UserProfile
 
+
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
 
