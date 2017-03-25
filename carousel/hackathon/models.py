6c91747a1693828ab5e711b55e36abaf41067921
Sebastian Morawiec <sebmorawiec@gmail.com>
2017-03-25 12:46:33 +0100

    added recruiter model

--- carousel/hackathon/models.py
+++ carousel/hackathon/models.py
@@ -37,7 +37,15 @@ class FacebookProfile(models.Model):
     profile_url = models.CharField(max_length=50)
     access_token = models.CharField(max_length=100)
 
+
+class Recruiter(models.Model):
+    name = models.CharField(max_length=100, default='')
+    surname = models.CharField(max_length=100, default='')
+    company_name = models.CharField(max_length=100, default='')
+
+
 class Offer(models.Model):
     lower_cash = models.IntegerField()
     higher_cash = models.IntegerField()
     location = models.CharField(max_length=100, default='')
+    recruiter = models.ForeignKey(Recruiter, default=0)
